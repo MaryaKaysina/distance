396d70f3b70045402bb9f73ca3cac8f80ec9c339
MaryaKaysina <61281550+MaryaKaysina@users.noreply.github.com>
2020-06-16 01:22:49 +0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,12 @@
 const menuBtn = document.querySelector('.m-menu');
 const menu = document.querySelector('.menu-sidebar');
+const share = document.querySelector('.post-button--share');
+const popoverMenu = document.querySelector('.popover_menu');
 
 menuBtn.addEventListener('click', function() {
     menu.classList.toggle('active');
+});
+
+share.addEventListener('click', function() {
+    popoverMenu.classList.toggle('menu_active');
 });
