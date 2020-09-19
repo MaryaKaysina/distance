758e48e55a6697a034cf8cd251695d0745637621
MaryaKaysina <61281550+MaryaKaysina@users.noreply.github.com>
2020-09-19 23:19:54 +0500

    final changes

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ const menuBtn = document.querySelector('.m-menu');
 const menu = document.querySelector('.menu-sidebar');
 const share = document.querySelector('.post-button--share');
 const popoverMenu = document.querySelector('.popover_menu');
+const createPost = document.querySelector('.create-post-input');
 
 menuBtn.addEventListener('click', function() {
     menu.classList.toggle('active');
@@ -9,4 +10,8 @@ menuBtn.addEventListener('click', function() {
 
 share.addEventListener('click', function() {
     popoverMenu.classList.toggle('menu_active');
+});
+
+createPost.addEventListener('click', function() {
+    createPost.innerHTML = '';
 });
